1cccda4d22f76ff35057d83746e83566d6ddd838
rogezeta <r.ortigosa@swansea.ac.uk>
2018-05-25 12:58:16 +0100

    Mex files in separated folder

--- code/build.m
+++ code/build.m
@@ -5,7 +5,9 @@ if ispc
     blas_lib = '-lmwblas';
     mex_ext  = 'mexw64';
 elseif isunix
-    blas_lib = '-lmblas';
+%     blas_lib = '-lmblas';
+    blas_lib = '-lblas';
+
     if ismac
         mex_ext  = 'mexmaci64';
     else
@@ -36,9 +38,9 @@ for ifile=1:size(cfunctions,1)
        c_files{aux}  =  filename;
        c_file_names{aux}  =  parts(1);
        if strcmp(parts(2),'c')
-           compiler{aux} = 'gcc';
+           compiler{aux} = 'gcc ';
        else
-           compiler{aux} = 'g++';
+           compiler{aux} = 'g++ -std=c++14';
        end
     end
 end
@@ -63,18 +65,34 @@ if mex_available==false
         inc_dir    = fullfile(matlabroot,'extern','include');
 %         code_include  =  'C:\SoftwareDevelopment\MECHANICAL_SOLID_CODE_OPTIMISED\code\include';
         code_dir   = fullfile(cdir,'code','include');
-        fastor_dir = fullfile(cdir,'code','include');
+        fastor_dir = fullfile(cdir,'code','include','Fastor-master');
 %         lib_dir  =  'C:\Program Files\MATLAB\R2015b\bin\win64';
+switch computer
+    case('GLNXA64')
+        lib_dir    = fullfile(matlabroot,'bin','glnxa64');
+    otherwise
         lib_dir    = fullfile(matlabroot,'bin','win64');
+end
         libraries  = [' -lmex -lmat -leng -lmx '];
 %         blas_path  =  'C:\Program Files\MATLAB\R2015b\extern\lib\win64\mingw64';
+switch computer
+    case('GLNXA64')
+        blas_path  =  '/usr/lib/libblas';
+    otherwise
         blas_path  =  fullfile(matlabroot,'extern','lib','win64','mingw64');
+end
+
         if strcmp(instruction,'release')
             opt_flags  =  ' -O3 -march=native -DNDEBUG';
         else   
             opt_flags = '';
         end
-        compilation_instruction  = [compile ' -I"'  inc_dir '" -I"' code_dir '"  -I"' fastor_dir '" -L"' lib_dir '"' libraries ' -L"' blas_path '" ' blas_lib opt_flags];
+        
+        fastorpath=  fullfile(pwd,'Fastor-master');
+        
+        
+         compilation_instruction  = [compile ' -fPIC' ' -I"'  inc_dir '" -I"' code_dir '"  -I"' fastor_dir  '" -L"' lib_dir '"' libraries ' -L"' blas_path '" ' blas_lib opt_flags ];
+
         system(compilation_instruction);
     end
 end
